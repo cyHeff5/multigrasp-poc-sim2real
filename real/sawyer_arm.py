162a9e7b1a85dddee2f25347d459f831baba0d20
cyHeff5 <leonpaul555@gmail.com>
2026-01-22 14:17:02 +0100

    Stuff

--- real/sawyer_arm.py
+++ real/sawyer_arm.py
@@ -13,8 +13,7 @@ class SawyerArm:
         self.limb = intera_interface.Limb(limb_name)
 
     def joint_angles(self) -> Dict[str, float]:
-        for joint in self.limb.joint_angles():
-            print(joint)
+        print(self.limb.joint_angles())
         return self.limb.joint_angles()
 
     def move_to_neutral(self, timeout: Optional[float] = None):
